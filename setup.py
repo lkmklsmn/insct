3b82e9bf93b8b309ed23bdd44b78995f3cb98b1c
yywang890107 <50466716+yywang890107@users.noreply.github.com>
2020-04-22 17:09:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 
 setuptools.setup(
-	name="bbtnn",version="0.0.1",description="Identify driving transcriptional regulator in single cell embeddings",author='Lukas Simon, Fangfang Yan',author_email="Lukas.Simon@uth.tmc.edu",packages=['bbtnn'],install_requires=['sklearn','scanpy','anndata', 'bbknn','pandas','seaborn','matplotlib','dca','tensorflow','numpy', 'ivis', 'scipy']
+	name="tnn",version="0.0.1",description="Deep triplet neural networks overcome batch effects in single cell sequencing data",author='Lukas Simon, Yin-Ying Wang',author_email="Lukas.Simon@uth.tmc.edu",packages=['tnn'],install_requires=['sklearn','scanpy','anndata', 'bbknn','pandas','seaborn','matplotlib','tensorflow','numpy', 'ivis', 'scipy']
 )
