1f7b1098c2c60e18b8d1026b36d3c8c20c1a4bc6
lkmklsmn <lkmklsmn@gmail.com>
2020-04-23 11:37:12 -0600

    sllim down install requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
 
 setuptools.setup(
-	name="tnn",version="0.0.1",description="Deep triplet neural networks overcome batch effects in single cell sequencing data",author='Lukas Simon, Yin-Ying Wang',author_email="Lukas.Simon@uth.tmc.edu",packages=['tnn'],install_requires=['sklearn','scanpy','anndata', 'bbknn','pandas','seaborn','matplotlib','tensorflow','numpy', 'ivis', 'scipy']
+	name="tnn",
+	version="0.0.1",
+	description="Deep triplet neural networks for integration of scRNAseq data",
+	author='Lukas Simon, Yin-Ying Wang',
+	author_email="lkmklsmn@gmail.com",
+	packages=['tnn'],
+	install_requires=['sklearn','scanpy','anndata','pandas','tensorflow','numpy', 'ivis', 'scipy', 'hnswlib']
 )
